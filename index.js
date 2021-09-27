6b225ad2aa17d15efb385e23e4dbeecab02db06f
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-09-27 14:47:11 -0700

    scrapped the guardian

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ const app = express();
 
 const url = "https://www.theguardian.com/uk";
 
-axios(url).then((response) => {
-  const html = response.data;
-  const $ 
-});
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
